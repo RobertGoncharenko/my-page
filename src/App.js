175393628ce8b7ab153fdf62f4d58290aee08fee
Robert Goncharenko <goncharenkorobert@gmail.com>
2023-08-10 12:06:58 +0200

    navbar(offcanvas)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserSearch from "./components/SearchUser";
-import BasicExample from './components/Navbar';
+import Example from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Example />
+      
       <div>
-        <>
-        <BasicExample />
-        </>
+        
         <UserSearch />
       </div>
     </div>
